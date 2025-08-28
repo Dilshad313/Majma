bb84939554d7391dc99ff1a102db54d2291172f1
Dilshad K <153605450+Dilshad313@users.noreply.github.com>
2025-08-28 22:04:59 +0530

    add the command

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+
+// Routes
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
